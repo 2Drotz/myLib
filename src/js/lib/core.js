61f4bd93300c1f245292d61b9b6b65b40a11d6a2
Dmitry <mandrin2013@mail.ru>
2022-09-10 19:47:44 +0300

    methods

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -1,3 +1,20 @@
 (() => {
+   const $ = function (selector) {
+      const elements = document.querySelectorAll(selector);
+      const obj = {};
 
+      obj.hide = () => {
+         elements.forEach(elem => {
+            elem.style.display = 'none';
+         });
+      };
+
+      obj.show = () => {
+         elements.forEach(elem => {
+            elem.style.display = '';
+         });
+      };
+   };
+
+   window.$ = $;
 })();
