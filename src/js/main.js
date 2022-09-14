6a3fd5b09a7a060f91009ba2520e1d82cb3016f1
Dmitry <mandrin2013@mail.ru>
2022-09-14 16:57:33 +0300

    co

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,12 @@ import './lib/lib';
 import $ from './lib/lib';
 
 $('button').on('click', function () {
-   $(this).toggleClass('active');
+   $('div').eq(2).toggleClass('active');
 });
+
+$('div').click(function () {
+   console.log($(this).index());
+});
+// console.log($('button').html());
+
+console.log($('div').eq(3).find('.some'));
