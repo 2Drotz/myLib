de80eac7ea970d863ce550de82818622d30fc242
Dmitry <mandrin2013@mail.ru>
2022-09-15 12:13:23 +0300

    commit

---
+++ src/js/lib/modules/effects.js
@@ -0,0 +1,34 @@
+
+import $ from '../core';
+
+$.prototype.animateOverTime = function (dur, callback, fin) {
+   let timeStart;
+
+   function _animateOverTime(time) {
+      if (!timeStart) {
+         timeStart = time;
+      }
+
+      let timeElapsed = time - timeStart;
+
+      let complection = Math.min(timeElapsed / dur, 1);
+
+      callback(complection);
+
+      if (timeElapsed < dur) {
+         requestAnimationFrame(_animateOverTime);
+      } else {
+         if (typeof fin === 'function') {
+            fin();
+         }
+      }
+   }
+
+   return _animateOverTime;
+};
+
+$.prototype.fadeIn = function (dur, display, fin) {
+   for (let i = 0; i < this.length; i++) {
+      this[i].style.display = display || 'block';
+   }
+}
