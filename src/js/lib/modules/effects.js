36c8c058d885f1882da98ed63baa8768933e6eeb
Dmitry <mandrin2013@mail.ru>
2022-09-16 14:27:01 +0300

    scss

--- src/js/lib/modules/effects.js
+++ src/js/lib/modules/effects.js
@@ -30,5 +30,34 @@ $.prototype.animateOverTime = function (dur, callback, fin) {
 $.prototype.fadeIn = function (dur, display, fin) {
    for (let i = 0; i < this.length; i++) {
       this[i].style.display = display || 'block';
+
+
+      const _fadeIn = (complection) => {
+         this[i].style.opacity = сomplection;
+      };
+
+      const ani = this.animateOverTime(dur, _fadeIn, fin);
+      requestAnimationFrame(ani);
    }
-}
+   return this;
+};
+
+
+$.prototype.fadeOut = function (dur, fin) {
+   for (let i = 0; i < this.length; i++) {
+
+
+
+      const _fadeOut = (complection) => {
+         this[i].style.opacity = 1 - complection;
+         if (complection === 1) {
+            this[i].style.display = 'none';
+         }
+
+      };
+
+      const ani = this.animateOverTime(dur, _fadeOut, fin);
+      requestAnimationFrame(ani);
+   }
+   return this;
+};
