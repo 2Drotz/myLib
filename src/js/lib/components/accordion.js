99ffa3feb70e1d6f93efdcece105947339c8d60e
Dmitry <mandrin2013@mail.ru>
2022-09-21 17:37:19 +0300

    sdf

---
+++ src/js/lib/components/accordion.js
@@ -0,0 +1,10 @@
+import $ from '../core';
+
+$.prototype.accordion = function (headActive = 'accordion-head-active',
+   contentActive = 'accordion-content--active', paddings = 40) {
+   for (let i = 0; i < this.length; i++) {
+      $(this[i]).click(() => {
+
+      });
+   }
+};
